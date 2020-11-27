805b8ecff36cd5735b8d6d697b5b856d01bdc69c
meklit <meklit_teshome@diveintocode.jp>
2020-11-27 12:14:33 +0900

    user&session edited

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    end
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        redirect_to posts_path
+        else
+        flash[:danger] = 'Failed to login'
+        render :new
+        end
+    end
+    def destroy
+        session.delete(:user_id)
+        flash[:notice] = 'You logged out'
+        redirect_to new_session_path
+    end
 end
