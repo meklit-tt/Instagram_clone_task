9ac3b4abbf50e51b5fef7e8af7ab4116dc74f715
meklit <meklit_teshome@diveintocode.jp>
2020-11-27 12:34:13 +0900

    model edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_many :favorites, dependent: :destroy
+ validates :name,  presence: true, length: { maximum: 30 }
+ validates :email, uniqueness: true, presence: true, length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+ before_validation { email.downcase! }
+    has_secure_password
+ validates :password, presence: true, length: { minimum: 6 }
+ has_many :favorites, dependent: :destroy
 end
