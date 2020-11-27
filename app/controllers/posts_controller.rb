6a4f65ec7a0d01a54c2c44f617f98c229bf5a901
meklit <meklit_teshome@diveintocode.jp>
2020-11-27 16:28:29 +0900

    design edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,73 +1,73 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @posts = Post.all
-    end
-
-    def show
-      @favorite = current_user.favorites.find_by(blog_id: @blog.id)
-    end
-
-    def new
-      if params[:back]
-        @post = Post.new(post_params)
-      else
-        @post = Post.new
-      end
-    end
-
-    def confirm
-      @post = Post.new(post_params)
-      @post.user_id = current_user.id
-    end
-
-    def edit
-    end
-
-    def create
-      @post = Post.new(post_params)
-      @post.user_id = current_user.id
-
-      respond_to do |format|
-        if @post.save
-          format.html { redirect_to @post, notice: 'Post was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-        else
-          format.html { render :new }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
-    def update
-      @post = Post.find(params[:id])
-      respond_to do |format|
-        if @post.update(post_params)
-          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-          format.json { render :show, status: :ok, location: @post }
-        else
-          format.html { render :edit }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
-    def destroy
-      @post.destroy
-      respond_to do |format|
-        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
-
-    private
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    def post_params
-      params.require(:post).permit(:image, :image_cache, :content)
-    end
-
+  def index
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.show(post_params)
+    @favorite = current_user.favorites.find_by(post_id: @post.id)
+  end
+
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_post
+     @post = Post.find(params[:id])
+  end
+
+  def post_params
+     params.require(:post).permit(:image, :image_cache, :content)
+  end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,11 @@ require("channels")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+// Read Sprockets README (https://github.com/rails/XXXXXXXXXXXXXXXXXXXXXX) for details
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require rails-ujs
+//= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
+//= require_tree
