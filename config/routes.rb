8618e8029d530e3d51182df5f29606de9ca03cc0
meklit <meklit_teshome@diveintocode.jp>
2020-11-27 13:55:59 +0900

    gem&bootstra pedited

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-resources :favorites, only: [:create, :destroy]
-end
+    root 'sessions#new'
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    resources :users
+    resources :posts do
+      collection do
+        post :confirm
+      end
+    end
+  end
