58eb3362307954b7afdcdd64c7b716db36f4bf80
meklit <meklit_teshome@diveintocode.jp>
2020-12-04 12:46:17 +0900

    css/helper prevent authomatic generation configured

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.generators.assets = false
+  config.generators.helper = false
   # Show full error reports.
   config.consider_all_requests_local = false
 
